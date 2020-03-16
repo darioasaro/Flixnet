14f1554bce2a615f28d3892a043bf30384484743
dario <darioasaro29@gmail.com>
2020-03-16 09:14:50 -0300

    integracionLogin

--- src/services/users.js
+++ src/services/users.js
@@ -3,6 +3,21 @@ export const getUsers = async () => {
     .then(response => response.json())
     .then(data => data);
 };
+export const login = async(user)=>{
+  var response =  await fetch('http://localhost:3000/api/auth/login',{
+    method : 'POST',
+    headers:{
+      'content-type':'application/json' 
+    },
+    body:JSON.stringify({
+      'username': user.username,
+      'password' : user.password
+    })
+  })
+  var data = await response.json()
+  return data
+  
+}
 
 export const checkUsers = () => {
   return localStorage.getItem("username") ? true : false;
